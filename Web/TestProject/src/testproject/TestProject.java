1cb26ebcc0f23a9eff7a7a4d37ae1316ef7b1e4c
Armando Ibarrarán <46429550+AI-2003@users.noreply.github.com>
2024-09-30 20:11:10 -0600

    last_class_updates

---
+++ Web/TestProject/src/testproject/TestProject.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testproject;
+
+/**
+ *
+ * @author carlo
+ */
+public class TestProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float x1 = 10.0;     
+        System.out.println("Valid "+ x1);
+        
+    }
+    
+}
